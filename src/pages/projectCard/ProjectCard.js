3d35680a305407f7a5bfd0f11d40fef6d4ebc3cc
nikolassco <scnoliveira59@gmail.com>
2022-08-11 20:25:52 -0300

    adding style to images

--- src/pages/projectCard/ProjectCard.js
+++ src/pages/projectCard/ProjectCard.js
@@ -1,8 +1,7 @@
 import React from 'react';
-
 import { useParams } from "react-router-dom";
-
 import { projectsList } from '../../data/projects';
+import styles from './ProjectCard.module.css';
 
 
 const ProjectCard = () => {
@@ -11,14 +10,24 @@ const ProjectCard = () => {
   const numberId = Number.parseInt(id, 10);
 
   return (
-    <div>
+    <div className={styles.container}>
       {projectsList.filter((project) => project.id === numberId).map((project) => (
-        <div key={project.id}>
-          <p>{project.name}</p>
-          <p>{project.explanation}</p>
-          <p>{project.githubUrl}</p>
-          <p>{project.pageLink}</p>
-          <p>{project.img}</p>
+        <div key={project.id} className={styles.project}>
+          <h2>{project.name.toUpperCase()}</h2>
+          <div className={styles.project_text}>
+            <p>{project.explanation}</p>
+          </div>
+          <div className={styles.project_links}>
+            <div className={styles.project_link}>
+              <p>Clique para ver o repositório no GitHub</p>
+              <a href={project.githubUrl} className='btn-redirect'>GitHub</a>
+            </div>
+            <div className={styles.project_link}>
+              <p>Clique para acessar ao site e testar sua funcionalidade</p>
+              <a href={project.pageLink} className='btn-redirect'>Site</a>
+            </div>
+          </div>
+          <img src={project.img} alt="Layout da Página" />
         </div>
       ))}
     </div>
